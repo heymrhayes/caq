ff1ce109162ed63dacde8475a9d409b585681952
David Hayes <heymrhayes@gmail.com>
2018-10-21 16:37:25 +0000

    groups fix - really

--- app-data.js
+++ app-data.js
@@ -2,5 +2,6 @@ var app = {
     "name": "Class Activity Questions",
     "endpoint": "https://script.google.com/macros/s/AKfycbzHgJS6hfEGS0CVmIFQ5JdnMrXNwyEVOn6W-mbOQWTs0q_1DJY/exec",
     "instructorsLoadedP": false,
-    "problemListLoadedP": false
+    "problemListLoadedP": false,
+    "myGroups": []
     }
