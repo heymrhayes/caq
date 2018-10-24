66879abb3f9ae72a1513033cc2ccd522c1817848
David Hayes <heymrhayes@gmail.com>
2018-10-24 19:02:41 +0000

    minor updates

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-var cacheName = 'caq-v1022';
+var cacheName = 'caq-v3';
 var appShellFiles = [
  "./index.html",
  "./script.js",
  "./style.css",
  "./loading.gif"
 ];
+
+var dataEndpointUrl = "https://script.google.com/macros/s/AKfycbzHgJS6hfEGS0CVmIFQ5JdnMrXNwyEVOn6W-mbOQWTs0q_1DJY/exec";
 
 
 self.addEventListener('install', function(e) {
@@ -21,12 +23,14 @@ self.addEventListener('install', function(e) {
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(r) {
-          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      console.log('[Service Worker] Fetching resource: '+ e.request.url);
+  
+      
       return r || fetch(e.request).then(function(response) {
-                return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
+          return caches.open(cacheName).then(function(cache) {
+            console.log('[Service Worker] Caching new resource: '+e.request.url);
+            cache.put(e.request, response.clone());
+            return response;
         });
       });
     })
