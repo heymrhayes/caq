ab6c6864c899c20d5c786a891f7f35577193ead1
David Hayes <heymrhayes@gmail.com>
2019-01-07 17:01:45 +0000

    cache management

--- sw.js
+++ sw.js
@@ -1,16 +1,37 @@
-var cacheName = 'caq-v181029';
+var cacheName = 'caq-v190107-1';
 var appShellFiles = [
  "./index.html",
  "./script.js",
  "./style.css",
- "./loading.gif"
+ "./loading.gif",
+ "./screen-content/_home.html"
 ];
 
 var dataEndpointUrl = "https://script.google.com/macros/s/AKfycbzHgJS6hfEGS0CVmIFQ5JdnMrXNwyEVOn6W-mbOQWTs0q_1DJY/exec";
 
 
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          console.log(cacheName)
+          return false;
+        }).map(function(cacheName) {
+          console.log("delete " + cacheName)
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+
+
+
 self.addEventListener('install', function(e) {
   console.log('[Service Worker] Install');
+  
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
           console.log('[Service Worker] Caching all: app shell and content');
@@ -21,17 +42,21 @@ self.addEventListener('install', function(e) {
 
 
 self.addEventListener('fetch', function(e) {
+  
   e.respondWith(
     caches.match(e.request).then(function(r) {
       console.log('[Service Worker] Fetching resource: '+ e.request.url);
   
       
       return r || fetch(e.request).then(function(response) {
-          return caches.open(cacheName).then(function(cache) {
-            console.log('[Service Worker] Caching new resource: '+e.request.url);
-            cache.put(e.request, response.clone());
-            return response;
-        });
+          return response;
+        
+        //   don't want to catch data 
+        //   return caches.open(cacheName).then(function(cache) {
+        //     console.log('[Service Worker] Caching new resource: '+e.request.url);
+        //     cache.put(e.request, response.clone());
+        //     return response;
+        // });
       });
     })
   );
